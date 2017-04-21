8eddebeaa48a55e31fd69234d67ee55d5d8501b8
Dan Jacob <dansamueljacob@gmail.com>
2017-04-21 14:48:18 -0700

    add second spec for case sensitivity

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,4 +5,7 @@ describe('String#anagram') do
   it('will check if inputted strings are anagrams') do
     expect(("ruby").anagram("bury")).to(eq("These words return an anagram!"))
   end
+  it('account for arbitrary capitalizations in strings') do
+    expect(("ruby").is_case_sensitive?()).to(eq(true))
+  end
 end
