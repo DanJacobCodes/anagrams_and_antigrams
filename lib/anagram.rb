eb04df90bbb472c92600027f4d0c41418fabd212
Dan Jacob <dansamueljacob@gmail.com>
2017-04-23 19:07:58 -0700

    rework anagram logic

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,41 +1,11 @@
 class String
   define_method(:anagram) do |word2|
-    first_word = self.split()
-    second_word = self.split()
-    if first_word == second_word
-      result = "These words return an anagram!"
-    else
-      result = "This is not an anagram"
+    first_word = self
+    second_word = word2
+    if first_word.chars.sort == word2.chars.sort && first_word.chars.sort.length == second_word.chars.sort.length
+      "These words are anagrams!"
+  elsif first_word != second_word
+  "These words are not anagrams!"
       end
     end
   end
-
-class String
-  define_method(:is_case_sensitive?) do
-    first_word = self.downcase().delete(' ')
-    second_word = first_word
-    if second_word == first_word
-      true
-    else
-     false
-    end
-  end
-end
-#
-# class String
-#   define_method(:is_palindrome?) do
-#     first_word = self.delete(' ')
-#     second_word = first_word.split("")
-#     reverse_array = []
-#     while second_word.length > 0 do
-#       letter = second_word.pop()
-#       reverse_array.push(letter)
-#     end
-#     first_word = reverse_array.join()
-#     if second_word == first_word
-#       "These words are palindromes!"
-#     else
-#      false
-#       end
-#     end
-#   end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -11,4 +11,22 @@ describe('String#anagram') do
   it('recognizes if phrase is a palindrome') do
     expect(().is_palindrome?()).to(eq("These words are palindromes!"))
   end
+  it('recognizes if inputs are actual words containing a vowel') do
+    expect(().is_palindrome?()).to(eq("These words are palindromes!"))
+  end
+  it('recognizes if word or phrase is actually an antigram') do
+    expect(().is_palindrome?()).to(eq("These words are palindromes!"))
+  end
+  it('accounts for multiple words being anagrams or antigrams') do
+    expect(().is_palindrome?()).to(eq("These words are palindromes!"))
+  end
 end
+
+
+
+
+# Add a rule to check if the inputs are words. Words can only be anagrams or anagrams if they are, in fact, actually words. (For instance, "yurb" isn't an anagram of "ruby" because "yurb" isn't a real word.) A word must contain a vowel (a, i, e, o, u) or y. Otherwise it's not a word. If one of the inputs has no vowels, your method should return something like this: "You need to input actual words!"
+#
+# If phrases aren't anagrams, the method should check whether they are actually "antigrams." For our purposes, a word is an "antigram" of another word if no letters match. For example, "hi" has no matches with "bye". The method should return something like this if there are no letter matches: "These words have no letter matches and are antigrams."
+#
+# Account for multiple words being anagrams or "antigrams." Spaces and punctuation shouldn't count (so they should be removed). You'll need to make sure that each word in the inputted phrases is really a word (passing condition #4 above). You may want to use a regular expression to remove additional chara
